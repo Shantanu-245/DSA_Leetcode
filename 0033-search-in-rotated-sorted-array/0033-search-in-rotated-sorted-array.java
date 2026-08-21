aa9a7b96811ab9da982a9065a257b64dd71b8cb4
Shantanu-245 <shantanuchauhan245@gmail.com>
2026-08-21 22:55:12 +0530

    Time: 0 ms (100%), Space: 43.6 MB (92.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int st = bs(nums,0,nums.length - 1);
-        int ed = nums.length - 1;
-        int ans = search(nums, 0,st - 1,target); // left half
-        if(ans == -1){
-            ans = search(nums, st, ed,target); // right half
-        }
-        return ans;
+        return BS(nums,0,nums.length - 1,target);
     }
-    static int bs(int[] arr,int st, int ed){
-        while(st < ed){
-            int md = (st + ed) /2;
-            if(arr[md] > arr[ed]){
-                st = md + 1;
-            }
-            else{
-                ed = md;
-            }
-        }
-        return st;
-    }
-    static int search(int[] arr,int st, int ed,int target){
+    static int BS(int[] arr, int st, int ed, int target){
         while(st <= ed){
-            int md = (st + ed) / 2;
+            int md = st + (ed - st) / 2;
             if(arr[md] == target){
                 return md;
             }
-            if(arr[md] > target){
-                ed = md - 1;
+            if(arr[st] <= arr[md]){
+                if(arr[st] <= target && target < arr[md]){
+                    ed = md - 1;
+                }
+                else{
+                    st = md + 1;
+                }
             }
             else{
-                st = md + 1;
+                if(arr[md] < target && target <= arr[ed]){
+                    st = md + 1;
+                }
+                else{
+                    ed = md - 1;
+                }
             }
         }
         return -1;
