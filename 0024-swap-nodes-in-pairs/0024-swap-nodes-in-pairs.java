0fbc8cb60e4c59badf6ee767352f22a092fd3cc7
Shantanu-245 <shantanuchauhan245@gmail.com>
2026-09-13 23:24:17 +0530

    Time: 0 ms (100%), Space: 43 MB (48.19%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode first = head, second = head.next, prev = null;
+        while(first != null && second != null){
+            ListNode third = second.next;
+            second.next = first;
+            first.next = third;
+            if(prev != null){
+                prev.next = second;
+            }
+            else{
+                head = second;
+            }
+            prev = first;
+            first = third;
+
+            if(third != null){
+                second = third.next;
+            }
+            else{
+                second = null;
+            }
+        }
+        return head;
+    }
+}
